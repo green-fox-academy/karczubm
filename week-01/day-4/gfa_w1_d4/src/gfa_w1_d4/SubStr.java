bd729c8cc54c5b2b2b41795e49268a3ca6ec15f2
karczubm <karczubka@gmail.com>
2019-09-13 14:10:46 +0200

    word searching excercise

--- week-01/day-4/gfa_w1_d4/src/gfa_w1_d4/SubStr.java
+++ week-01/day-4/gfa_w1_d4/src/gfa_w1_d4/SubStr.java
@@ -5,12 +5,28 @@
  */
 package gfa_w1_d4;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Gergely
  */
 public class SubStr {
+
+    public static int subStr(String input, String q) {
+        int intIndex = input.indexOf(q);
+        if (intIndex == -1) {
+            return -1;
+        } else {
+            return intIndex;
+        }
+    }
+
     public static void main(String[] args) {
-        
+        String ip1, ip2;
+        Scanner sc = new Scanner(System.in);
+        ip1 = sc.nextLine();
+        ip2 = sc.nextLine();
+        System.out.println(subStr(ip1, ip2));
     }
 }
