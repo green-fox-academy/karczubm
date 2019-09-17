c2719651fb55f33072540eca974ed71114918a8c
karczubm <karczubka@gmail.com>
2019-09-17 14:28:12 +0200

    list+map 1. exercises

--- week-02/day-2/src/ListInstructions1.java
+++ week-02/day-2/src/ListInstructions1.java
@@ -1,35 +1,55 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class ListInstructions1 {
     public static void main(String[] args) {
-    //Create an empty list which will contain names (strings)
-        ArrayList<String> names=new ArrayList<>();
-    //Print out the number of elements in the list
+        //Create an empty list which will contain names (strings)
+        ArrayList<String> names = new ArrayList<>();
+        //Print out the number of elements in the list
         System.out.println(names.size());
-    //Add William to the list
+        //Add William to the list
         names.add("William");
-    //Print out whether the list is empty or not
-        System.out.println("Is it empty? :"+names.isEmpty());
-    //Add John to the list
-        names.add("Jhon");
-    //Add Amanda to the list
+        //Print out whether the list is empty or not
+        System.out.println("Is it empty? :" + names.isEmpty());
+        //Add John to the list
+        names.add("John");
+        //Add Amanda to the list
         names.add("Amanda");
-    //Print out the number of elements in the list
+        //Print out the number of elements in the list
         System.out.println(names.size());
-    //Print out the 3rd element
+        //Print out the 3rd element
         System.out.println(names.get(2));
     /*Iterate through the list and print out each name
             William
             John
             Amanda*/
-        System.out.println(names.iterator());
+        /*A
+            System.out.println(names);*/
+
+        Iterator<String> it = names.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
     /*Iterate through the list and print
         1. William
         2. John
         3. Amanda*/
-    //Remove the 2nd element
+        int b = 1;
+        for (String i : names.toArray(new String[0])
+        ) {
+            System.out.println(b + ". " + i);
+            b++;
+        }
+        //Remove the 2nd element
+        names.remove(1);
     /*Iterate through the list in a reversed order and print out each name
             Amanda
             William*/
-    //Remove all elements
-}}
+        String[] namesArray = names.toArray(new String[0]);
+        for (int i = names.size() - 1; i >= 0; i--) {
+            System.out.println(namesArray[i]);
+        }
+        //Remove all elements
+        names.clear();
+    }
+}
---
+++ week-02/day-2/src/MapIntroduction1.java
@@ -0,0 +1,41 @@
+import java.util.HashMap;
+
+public class MapIntroduction1 {
+    public static void main(String[] args) {
+        //Create an empty map where the keys are integers and the values are characters
+        HashMap<Integer, String> myMap = new HashMap<>();
+        //Print out whether the map is empty or not
+        System.out.println(myMap.isEmpty());
+        //Add the following key-value pairs to the map
+        /*Key	Value
+        97	a
+        98	b
+        99	c
+        65	A
+        66	B
+        67	C*/
+        myMap.put(97, "a");
+        myMap.put(98, "b");
+        myMap.put(99, "c");
+        myMap.put(65, "A");
+        myMap.put(66, "B");
+        myMap.put(67, "C");
+
+        //Print all the keys
+        System.out.println(myMap.keySet());
+        //Print all the values
+        System.out.println(myMap.values());
+        //Add value D with the key 68
+        myMap.put(68,"D");
+        //Print how many key-value pairs are in the map
+        System.out.println(myMap.size());
+        //Print the value that is associated with key 99
+        System.out.println(myMap.get(99));
+        //Remove the key-value pair where with key 97
+        myMap.remove(97);
+        //Print whether there is an associated value with key 100 or not
+        System.out.println(myMap.containsKey(100));
+        //Remove all the key-value pairs
+        myMap.clear();
+    }
+}
