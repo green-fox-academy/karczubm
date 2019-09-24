190dd35223552497b2eedf778f24f025e7317867
karczubm <karczubka@gmail.com>
2019-09-24 12:22:35 +0200

    PrintEachLine

---
+++ week-03/day-2/src/PrintEachLine.java
@@ -0,0 +1,20 @@
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class PrintEachLine {
+    public static void main(String[] args) {
+        try {
+            Path filePath = Paths.get("assets/text.txt");
+            List<String> lines = Files.readAllLines(filePath);
+            for (String line : lines) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("File is missing!");
+        }
+    }
+}
