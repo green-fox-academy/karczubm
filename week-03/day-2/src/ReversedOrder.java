4bc09a6c4605bc82cdb8356eb74098b7de20a084
karczubm <karczubka@gmail.com>
2019-09-24 17:29:22 +0200

    Reversed order

---
+++ week-03/day-2/src/ReversedOrder.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReversedOrder {
+    public static void main(String[] args) {
+        reversedDescryter("reversed-order.txt");
+    }
+    public static void reversedDescryter(String fileName) {
+        Path filePath = Paths.get("assets", fileName);
+        List<String> descryptedFile = new ArrayList<>();
+        List<String> originalFile = new ArrayList<>();
+        try {
+            originalFile = Files.readAllLines(filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String newLine = "";
+        for (int i = originalFile.size(); i >0 ; i--) {
+            newLine = originalFile.get(i-1);
+            descryptedFile.add(newLine);
+            newLine = "";
+        }
+        try {
+            Files.write(filePath, descryptedFile);
+        } catch (Exception e) {
+        }
+
+}}
+
