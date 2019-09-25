dbd5a0e95141ec67ad71da2db7932d5dcaa24944
karczubm <karczubka@gmail.com>
2019-09-25 12:36:18 +0200

    BlogPost. 2nd ex. of classes

---
+++ week-03/day-3/src/blogPost/Main.java
@@ -0,0 +1,7 @@
+package blogPost;
+
+public class Main {
+    public static void main(String[] args) {
+        BlogPost bp=new BlogPost("John Doe","Lorem ipsum","Lorem ipsum dolor sitamet","2000.05.04.");
+    }
+}
